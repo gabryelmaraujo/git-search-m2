8a8e9eed236331660cdb36eceef5840c6e7d2793
Gabryel <gabryelmaraujo@gmail.com>
2022-10-12 12:47:05 -0300

    To refresh pause

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,69 +1,121 @@
-/* Desenvolva sua lógica aqui...*/
+// /* Desenvolva sua lógica aqui...*/
 
-const userNameInput = document.getElementById("userNameInput");
-const searchButton = document.querySelector(".searchButton");
+// const userNameInput = document.getElementById("userNameInput");
+// const searchButton = document.querySelector(".searchButton");
 
-async function getUserInfos(user) {
+// async function getUserInfos(user) {
 
-    const data = await fetch(`https://api.github.com/users/${user}`)
-    const dataJSON = await data.json()
-        console.log(dataJSON)
+//     const data = await fetch(`https://api.github.com/users/${user}`)
+//     const dataJSON = await data.json()
+//         console.log(dataJSON)
 
-    const userAvatar = dataJSON.avatar_url
-    const userName = dataJSON.name
-    const userBio = dataJSON.bio
-    const userURL = dataJSON.url
-    const userReposUrl = dataJSON.repos_url
+//     const userAvatar = dataJSON.avatar_url
+//     const userName = dataJSON.name
+//     const userBio = dataJSON.bio
+//     const userURL = dataJSON.url
+//     const userReposUrl = dataJSON.repos_url
 
-    const userObj = {
-        avatar: userAvatar,
-        name: userName,
-        bio: userBio,
-        url: userURL,
-        reposUrl: userReposUrl
-    }
+//     const userObj = {
+//         avatar: userAvatar,
+//         name: userName,
+//         bio: userBio,
+//         url: userURL,
+//         reposUrl: userReposUrl
+//     }
+
+//     if(recentUsersArr.length == 3){
+//         recentUsersArr.splice(0, 1)
+//         recentUsersArr.push(userObj)
+//     }else{
+//         recentUsersArr.push(userObj)
+//     }
+
+//     // console.log(recentUsersArr)
+        
+//     const recentUl = document.querySelector('.recentUl')
+//     const recentLi = document.querySelector('.recentIcon')
+//     if(recentUl.childElementCount == 3){
+//         recentUl.removeChild(recentLi)
+//     }
+
+    
+
+//         recentUl.insertAdjacentHTML('beforeend',`
+//         <li class = "recentIcon">
+//             <div class="recentImg">
+//                 <a href="${userURL}">
+//                     <img src="${userAvatar}">
+//                 </a>
+//                 <div class="anchorTextDiv" hidden>
+//                     <p>Acessar este perfil</p>
+//                 </div>
+
+//             </div>
+//         </li>
+//     `)
+
+
+
+// //REDIRECT ON CLICK 
+
+// const formAction = document.getElementById('searchForm')
+// formAction.action = '/pages/profile/index.html'
+
+//     const profileImg = document.querySelector('.profileImg')
+//     console.log(profileImg)
+
+// }
+
+// searchButton.addEventListener('click', (e)=>{
+//     // e.preventDefault()
+//     getUserInfos(userNameInput.value)
+
+// })
+
+
+
+//------------------------------------------------ OUTRA LÓGICA
+
+
+const searchButton = document.querySelector(".searchButton")
+
+
+searchButton.addEventListener('click', ((e)=>{
+    e.preventDefault()
+
+    toLocalStorage()
+}))
+
+async function toLocalStorage(){
+    const input = document.getElementById("userNameInput")
+    const userData = await getUserInfos(input.value)
 
     if(recentUsersArr.length == 3){
         recentUsersArr.splice(0, 1)
-        recentUsersArr.push(userObj)
+        recentUsersArr.push(userData)
     }else{
-        recentUsersArr.push(userObj)
+        recentUsersArr.push(userData)
     }
+    
+    
 
-    console.log(recentUsersArr)
-        
-    const recentUl = document.querySelector('.recentUl')
-        recentUl.insertAdjacentHTML('beforeend',`
-        <li class = "recentIcon">
-            <div class="recentImg">
-                <a href="${userURL}">
-                    <img src="${userAvatar}">
-                </a>
-                <div class="anchorTextDiv" hidden>
-                    <p>Acessar este perfil</p>
-                </div>
-
-            </div>
-        </li>
-    `)
-
+ localStorage.setItem("@GitSearch/UserData:", JSON.stringify(recentUsersArr))
 }
 
-searchButton.addEventListener('click', (e)=>{
-    e.preventDefault()
-    getUserInfos(userNameInput.value)
-})
 
-// const recentUl = document.querySelector('.recentUl')
-// if(recentUl.childElementCount != 0){
-//     const recentAnchor = document.querySelector('.recentAnchor')
+async function getUserInfos(user){
 
-//     recentAnchor.addEventListener('mouseover', ()=>{
-//         const anchorTextDiv = document.querySelector('.anchorTextDiv')
-//         anchorTextDiv.hidden = false
-//     })
-//     recentAnchor.addEventListener('mouseout', ()=>{
-//         const anchorTextDiv = document.querySelector('.anchorTextDiv')
-//         anchorTextDiv.hidden = true
-//     })
-// }
+    const data = await fetch(`https://api.github.com/users/${user}`)
+    const dataJSON = await data.json()
+
+    const userInfos = {
+        userAvatar: dataJSON.avatar_url,
+        userName: dataJSON.name,
+        userBio: dataJSON.bio,
+        userURL: dataJSON.url,
+        userReposUrl: dataJSON.repos_url
+    }
+
+    return userInfos
+
+}
--- scripts/recentusers.js
+++ scripts/recentusers.js
@@ -1,24 +1,38 @@
 
-let recentUsersArr = [
-    {
-        avatar: 'teste',
-        name: 'teste',
-        bio: 'teste',
-        url: 'teste',
-        reposUrl: 'teste'
-    }
-]
+let recentUsersArr = []
 
-function toLocalStorage(){
+function getRecentSearchs(){
 
-    if(recentUsersArr.length > 0){
-        recentUsersArr.forEach((e)=>{
-            let jsonElement = JSON.stringify(e)
-            localStorage.setItem("teste", jsonElement)
+    const usersArr = JSON.parse(localStorage.getItem("@GitSearch/UserData:"))
+    
+    if(usersArr.length > 0){
+        usersArr.forEach((user)=>{
+            console.log(user)
+            const avatar = user.userAvatar
+            const name = user.userName
+            const bio = user.userBio
+            const url = user.userURL
+            const repos = user.userReposUrl
+
+            const recentUl = document.querySelector('.recentUl')
+            recentUl.innerHTML=''
+
+            recentUl.insertAdjacentHTML('beforeend', `
+                <li class="recentIcon">
+                    <div class="recentImg">
+                        <a href="/pages/profile/index.html">
+                            <img src="${avatar}">
+                        </a>
+                        <div class="anchorTextDiv" hidden>
+                            <p>Acessar este perfil</p>
+                        </div>
+
+                    </div>
+                </li>
+
+            `)
 
         })
     }
-
-
 }
-toLocalStorage()
+getRecentSearchs()
